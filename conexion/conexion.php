5404ddf64b7dc064ef12f8c7be03b74d11c9b812
WARG1019 <eldioswarg@gmail.com>
2018-08-01 13:22:33 -0500

    Revert "cambio en el archivo originar index.html a index.php"
    This reverts commit 3e7a8a404ef58ef7c1cab3d22228d0adead323f9.

--- conexion/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-$local="localhost";
-$root="root";
-$contra ="warg";
-$bda = "u465272718_cite";
-$conexion = @mysql_connect($local,$root,$contra);
-@mysql_select_db($bda,$conexion)or die ("falla la conexion  db1");
-?>
